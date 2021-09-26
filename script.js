21c220e589f6f18aaab86e82b4f43ff86eff07ef
veggiepilot <johanneschitura@gmail.com>
2021-09-26 07:12:50 -0400

    Added searched city history buttons functionality

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let city;
 const searchButton  = document.getElementById("search");
 const cityInput     = document.getElementsByClassName("form-control");
 const searchHistory = document.getElementById("search-history");
-console.log(cityInput[0].value);
 
 // fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
 //         + city + 
@@ -16,7 +15,14 @@ console.log(cityInput[0].value);
 
 // Search button event listener
 searchButton.addEventListener("click", function() {
-    console.log(searchHistory);
+    let searchHistoryButton = searchHistory.appendChild(document.createElement("button"));
+    searchHistoryButton.setAttribute('type', 'button');
+    searchHistoryButton.classList.add("btn");
+    searchHistoryButton.classList.add("btn-secondary");
+    searchHistoryButton.classList.add("w-100");
+    searchHistoryButton.classList.add("mt-3");
+    searchHistoryButton.setAttribute('id', cityInput[0].value);
+    searchHistoryButton.innerText = cityInput[0].value;
 }) 
 
 
